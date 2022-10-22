3b48a175f4be74caff7d40156dc7ec1a79c2a252
Riley Loudon <rileyloudon@icloud.com>
2022-10-21 23:13:20 -0500

    add temporary example data, fix fetch string

--- src/Components/Movie/Movie.tsx
+++ src/Components/Movie/Movie.tsx
@@ -47,6 +47,88 @@ const Movie: React.FC = () => {
     Response: 'Loading',
   });
 
+  // OMDB seems to having problems with thier api, here's a copy of season 1 westworld
+  const movieEpisodeData = {
+    Title: 'Westworld',
+    Season: '1',
+    totalSeasons: '4',
+    Episodes: [
+      {
+        Title: 'The Original',
+        Released: '2016-10-02',
+        Episode: '1',
+        imdbRating: 'N/A',
+        imdbID: 'tt4227538',
+      },
+      {
+        Title: 'Chestnut',
+        Released: '2016-10-09',
+        Episode: '2',
+        imdbRating: '8.5',
+        imdbID: 'tt4562758',
+      },
+      {
+        Title: 'The Stray',
+        Released: '2016-10-16',
+        Episode: '3',
+        imdbRating: '8.2',
+        imdbID: 'tt4625856',
+      },
+      {
+        Title: 'Dissonance Theory',
+        Released: '2016-10-23',
+        Episode: '4',
+        imdbRating: '8.6',
+        imdbID: 'tt4625862',
+      },
+      {
+        Title: 'Contrapasso',
+        Released: '2016-10-30',
+        Episode: '5',
+        imdbRating: '8.6',
+        imdbID: 'tt4625866',
+      },
+      {
+        Title: 'The Adversary',
+        Released: '2016-11-06',
+        Episode: '6',
+        imdbRating: '8.8',
+        imdbID: 'tt4630544',
+      },
+      {
+        Title: "Trompe L'Oeil",
+        Released: '2016-11-13',
+        Episode: '7',
+        imdbRating: '9.4',
+        imdbID: 'tt4630546',
+      },
+      {
+        Title: 'Trace Decay',
+        Released: '2016-11-20',
+        Episode: '8',
+        imdbRating: '8.7',
+        imdbID: 'tt4458814',
+      },
+      {
+        Title: 'The Well-Tempered Clavier',
+        Released: '2016-11-27',
+        Episode: '9',
+        imdbRating: '9.3',
+        imdbID: 'tt5220026',
+      },
+      {
+        Title: 'The Bicameral Mind',
+        Released: '2016-12-04',
+        Episode: '10',
+        imdbRating: '9.7',
+        imdbID: 'tt5229638',
+      },
+    ],
+    Response: 'True',
+  };
+
+  console.log(movieEpisodeData);
+
   useEffect(() => {
     (async (): Promise<void> => {
       const API_KEY: string = process.env.REACT_APP_API_KEY!;
@@ -57,7 +139,7 @@ const Movie: React.FC = () => {
       // http://www.omdbapi.com/?apikey=&t=Game%20of%20Thrones&Season=1&Episode=1
 
       const response = await fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${
+        `https://www.omdbapi.com/?apikey=${API_KEY}&t=${
           basicMovieInfo.Title || title || ''
         }`
       );
