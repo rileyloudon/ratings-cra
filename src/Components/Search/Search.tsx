c70ad2d6160cbfe220de9a30490cbad477712438
Riley Loudon <rileyloudon@icloud.com>
2022-10-23 21:48:23 -0500

    remove label

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -14,16 +14,13 @@ const Search: React.FC = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor='search'>
-          Search for Movies or TV Shows
-          <input
-            type='search'
-            id='search'
-            placeholder='Search'
-            value={searchString}
-            onChange={(e) => setSearchString(e.target.value)}
-          />
-        </label>
+        <input
+          type='search'
+          id='search'
+          placeholder='Search'
+          value={searchString}
+          onChange={(e) => setSearchString(e.target.value)}
+        />
         <button type='submit'>Search</button>
       </form>
     </div>
