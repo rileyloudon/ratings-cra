2aec1a88e9294b865e07e48c96aad090e34e6e1f
Riley Loudon <rileyloudon@icloud.com>
2022-11-01 23:22:34 -0500

    use ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { useMemo, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import SearchResults from './Components/SearchResults/SearchResults';
 import Movie from './Components/Movie/Movie';
 import Header from './Components/Header/Header';
+import ThemeContext from './Context/ThemeContext';
 import './App.css';
 
-const App: React.FC = () => (
+const App: React.FC = () => {
   // Dont build/use github pages -> use different hosting site then link in readme
   // Keep api key safe ^
 
@@ -17,15 +19,30 @@ const App: React.FC = () => (
   // const [movie, setMovie] = useState<unknown>();
   // const [error, setError] = useState<unknown>();
 
-  <div className='App'>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='*' element={<Home />} />
-        <Route path='search=:title' element={<SearchResults />} />
-        <Route path='title/:title' element={<Movie />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+  const fallbackTheme = window.matchMedia('(prefers-color-scheme: dark)')
+    .matches
+    ? 'dark'
+    : 'light';
+  const [theme, setTheme] = useState<string>(
+    localStorage.getItem('theme') || fallbackTheme
+  );
+
+  const changeTheme = useMemo(() => ({ theme, setTheme }), [theme]);
+
+  return (
+    <div className='App' data-theme={theme}>
+      <BrowserRouter>
+        <ThemeContext.Provider value={changeTheme}>
+          <Header />
+          <Routes>
+            <Route path='*' element={<Home />} />
+            <Route path='search=:title' element={<SearchResults />} />
+            <Route path='title/:title' element={<Movie />} />
+          </Routes>
+        </ThemeContext.Provider>
+      </BrowserRouter>
+    </div>
+  );
+};
+
 export default App;
