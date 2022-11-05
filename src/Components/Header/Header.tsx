0f8d020367650c6ff2ce7e337a8f9719772add3f
Riley Loudon <rileyloudon@icloud.com>
2022-11-04 22:55:38 -0500

    add functionality to toggle theme button

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,23 @@
+import { useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import ThemeContext from '../../Context/ThemeContext';
+import { ReactComponent as LightSvg } from '../../img/light-mode.svg';
+import { ReactComponent as DarkSvg } from '../../img/dark-mode.svg';
+
 import Search from '../Search/Search';
 import styles from './Header.module.css';
 
 const Header: React.FC = () => {
   const location = useLocation();
+  console.log(location);
+
+  const currentTheme = useContext(ThemeContext);
+
+  const handleTheme = () => {
+    const newTheme = currentTheme?.theme === 'light' ? 'dark' : 'light';
+    currentTheme?.setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
 
   return (
     <header className={styles.container}>
@@ -11,7 +25,8 @@ const Header: React.FC = () => {
         <Link to='/'>Ratings</Link>
       </h2>
       {location.pathname !== '/' && <Search />}
-      <button type='button' className={styles.button}>
+      <button type='button' className={styles.button} onClick={handleTheme}>
+        {currentTheme?.theme === 'light' ? <LightSvg /> : <DarkSvg />}
         Toggle Theme
       </button>
     </header>
