c93d84be108b0a5884d73c37f7b4146449427f51
Riley Loudon <rileyloudon@icloud.com>
2022-11-09 23:03:38 -0600

    pass updateSearch to Search

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,14 @@
 import Search from '../Search/Search';
 import styles from './Home.module.css';
 
-const Home = () => (
+interface HomeProps {
+  updateCurrentSearch(value: string): void;
+}
+
+const Home = ({ updateCurrentSearch }: HomeProps) => (
   <div className={styles.container}>
     <p>Search for Movies or TV Shows</p>
-    <Search />
+    <Search updateCurrentSearch={updateCurrentSearch} />
   </div>
 );
 
