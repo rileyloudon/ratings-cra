99e12598a4085ac298ef5de20cb350f39ae65a80
Riley Loudon <rileyloudon@icloud.com>
2022-11-11 21:22:38 -0600

    small change

--- src/Components/Spinner/Spinner.tsx
+++ src/Components/Spinner/Spinner.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as SpinnerSvg } from '../../img/spinner.svg';
 import styles from './Spinner.module.css';
 
-const Spinner: React.FC = () => <SpinnerSvg className={styles.spinner} />;
+const Spinner = () => <SpinnerSvg className={styles.spinner} />;
 
 export default Spinner;
