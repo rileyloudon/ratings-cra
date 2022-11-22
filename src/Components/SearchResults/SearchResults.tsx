753b38d52afd1d5d3c4a3a878362940b4bd178df
Riley Loudon <rileyloudon@icloud.com>
2022-11-21 22:29:49 -0600

    start switching from OMDB to TMDB

--- src/Components/SearchResults/SearchResults.tsx
+++ src/Components/SearchResults/SearchResults.tsx
@@ -4,29 +4,41 @@ import Spinner from '../Spinner/Spinner';
 import styles from './SearchResults.module.css';
 
 interface Results {
-  Response: string;
-  Search?: [
+  // TV SHOW
+  page?: number;
+  results?: [
     {
-      Poster: string;
-      Title: string;
-      type: string;
-      year: string;
-      imdbID: string;
+      backdrop_path: string;
+      first_air_date: string;
+      genre_ids: number[];
+      id: number;
+      media_type: string;
+      name: string;
+      origin_country: string[];
+      original_language: string;
+      original_name: string;
+      overview: string;
+      popularity: number;
+      poster_path: string;
+      vote_average: number;
+      vote_count: number;
     }
   ];
-  totalResults?: string;
-  Error?: string;
+  total_pages?: number;
+  total_results?: number;
+
+  // ERROR
+  status_message?: string;
+  status_code?: number;
 }
 
 const SearchResults = () => {
   const { title } = useParams();
 
-  const [results, setResults] = useState<Results>({
-    Response: 'Loading',
-  });
+  const [results, setResults] = useState<Results>({});
 
   const renderSearchResults = (): JSX.Element => {
-    if (results.Response === 'Loading')
+    if (!results)
       return (
         <>
           <Spinner />
@@ -34,27 +46,32 @@ const SearchResults = () => {
         </>
       );
 
-    if (results.Response === 'False') return <p>{results.Error}</p>;
+    if (results.status_message) return <p>{results.status_message}</p>;
 
     // const totalPages = Math.ceil(
     //   parseInt(results.totalResults || '1', 10) / 10
     // );
 
+    // tv shows use 'name', moveies use 'title'
     return (
       <>
-        {results.Search?.map((item) => (
+        {results.results?.map((item) => (
           <Link
-            to={`/title/${item.Title.toLowerCase()}`}
-            key={item.imdbID}
+            // to={`/title/${item.name.toLowerCase()}`}
+            to='/title'
+            key={item.id}
             state={item}
             className={styles['search-item']}
           >
-            {item.Poster !== 'N/A' ? (
-              <img src={item.Poster} alt={`${item.Title} Poster`} />
+            {item.poster_path !== null ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
+                alt={`${item.name} Poster`}
+              />
             ) : (
               <p className={styles['no-poster']}>No Poster</p>
             )}
-            <p>{item.Title}</p>
+            <p>{item.name}</p>
           </Link>
         ))}
         <div className={styles['page-selector']}>{/* Page Buttons */}</div>
@@ -66,19 +83,17 @@ const SearchResults = () => {
     (async (): Promise<void> => {
       const API_KEY: string = process.env.REACT_APP_API_KEY!;
 
-      // Returns specific episode, remove &episode=1 to get full season
-      // https://www.omdbapi.com/?apikey={API_KEY}&t=house+of+the+dragon&season=1&episode=1
-
       const response = await fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY}&s=${title || ''}&page=1`
+        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${
+          title || ''
+        }&page=1&include_adult=false`
       );
       const data = (await response.json()) as Results;
 
       console.log(data);
       setResults(data);
     })().catch((err: unknown) => {
-      if (err instanceof Error)
-        setResults({ Response: 'False', Error: err.message });
+      if (err instanceof Error) setResults({ status_message: err.message });
     });
   }, [title]);
 
