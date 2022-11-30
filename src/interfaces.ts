a504263bcece7763659ef68d917c81d34ccfc996
Riley Loudon <rileyloudon@icloud.com>
2022-11-29 21:54:59 -0600

    create interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,44 @@
+export interface TvShow {
+  poster_path: string | null;
+  popularity: number;
+  id: number;
+  overview: string;
+  backdrop_path: string | null;
+  vote_average: number;
+  media_type?: 'tv';
+  first_air_date: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
+  name: string;
+  original_name: string;
+}
+
+export interface Movie {
+  poster_path: string | null;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  original_title: string;
+  genre_ids: number[];
+  id: number;
+  media_type?: 'movie';
+  original_language: string;
+  title: string;
+  backdrop_path: string | null;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+export interface Person {
+  profile_path: string | null;
+  adult: boolean;
+  id: number;
+  media_type: 'person';
+  known_for: (TvShow | Movie)[];
+  name: string;
+  popularity: number;
+}
