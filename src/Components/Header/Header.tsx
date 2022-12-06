0f16b968992c3348ffd3726810cd540280ad925b
Riley Loudon <rileyloudon@icloud.com>
2022-12-05 21:18:36 -0600

    update regex to test ids

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -24,7 +24,9 @@ const Header = ({ currentSearch, updateCurrentSearch }: HeaderProps) => {
 
   const displaySearch =
     /\/search=[A-Za-z0-9]+/i.test(location.pathname) ||
-    /\/title\/[A-Za-z0-9]+/i.test(location.pathname);
+    /\/tvshow\/[0-9]+/i.test(location.pathname) ||
+    /\/movie\/[0-9]+/i.test(location.pathname) ||
+    /\/actor\/[0-9]+/i.test(location.pathname);
 
   return (
     <header className={styles.container}>
