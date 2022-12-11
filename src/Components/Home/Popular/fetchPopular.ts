b083ab43d728679074e77ae7612e3936508f24c0
Riley Loudon <rileyloudon@icloud.com>
2022-12-10 18:26:31 -0600

    add region to fetch calls

--- src/Components/Home/Popular/fetchPopular.ts
+++ src/Components/Home/Popular/fetchPopular.ts
@@ -1,53 +1,46 @@
-import { TvShow, Movie } from '../../../interfaces';
+import { TvShow, Movie, ApiError } from '../../../interfaces';
 
-interface PopularTV {
-  // Success
-  page?: 1;
-  results?: TvShow[];
-  total_results?: number;
-  total_pages?: number;
-
-  // Error
-  status_message?: string;
-  status_code?: number;
+interface TvResults {
+  page: 1;
+  results: TvShow[];
+  total_results: number;
+  total_pages: number;
 }
 
-interface PopularMovie {
-  // Success
-  page?: 1;
-  results?: Movie[];
-  total_results?: number;
-  total_pages?: number;
+type PopularTV = TvResults | ApiError;
 
-  // Error
-  status_message?: string;
-  status_code?: number;
+interface MovieResults {
+  page: 1;
+  results: Movie[];
+  total_results: number;
+  total_pages: number;
 }
+
+type PopularMovie = MovieResults | ApiError;
 
 const fetchPopular = async () => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
+  const countryCode = Intl.DateTimeFormat().resolvedOptions().locale.slice(-2);
 
   const tvResponse = await fetch(
-    `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1&region=${countryCode}`
   );
   const tvData = (await tvResponse.json()) as PopularTV;
+  if ('status_message' in tvData) return tvData.status_message;
 
   const movieResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1&region=${countryCode}`
   );
   const movieData = (await movieResponse.json()) as PopularMovie;
+  if ('status_message' in movieData) return movieData.status_message;
 
-  if (tvData.results && movieData.results) {
-    const combinedPopular: (Movie | TvShow)[] = [
-      ...tvData.results,
-      ...movieData.results,
-    ]
-      .sort((a, b) => b.popularity - a.popularity)
-      .slice(0, 10);
-    return combinedPopular;
-  }
-
-  return tvData.status_message || movieData.status_message;
+  const combinedPopular: (Movie | TvShow)[] = [
+    ...tvData.results,
+    ...movieData.results,
+  ]
+    .sort((a, b) => b.popularity - a.popularity)
+    .slice(0, 10);
+  return combinedPopular;
 };
 
 export default fetchPopular;
