a51a5dc6c85374275a043ec49c54877a0a33413f
Riley Loudon <rileyloudon@icloud.com>
2022-12-12 22:32:59 -0600

    small change

--- src/Components/DetailedView/Movie/fetchMovieData.ts
+++ src/Components/DetailedView/Movie/fetchMovieData.ts
@@ -5,7 +5,7 @@ type MovieData = Movie | ApiError;
 const fetchMovieData = async (movieId: string | undefined) => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
 
-  if (!movieId) return { status_code: 0, status_message: 'No movieId found' };
+  if (!movieId) return { status_code: 0, status_message: 'No movieID found' };
 
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
