48091ef234509b3e55f2d1dd05592996411daa3f
Riley Loudon <rileyloudon@icloud.com>
2022-12-13 22:20:29 -0600

    update to use ApiError

--- src/Components/SearchResults/fetchSearchResults.ts
+++ src/Components/SearchResults/fetchSearchResults.ts
@@ -1,26 +1,23 @@
-import { Movie, Person, TvShow } from '../../interfaces';
+import { ApiError, Movie, Person, TvShow } from '../../interfaces';
 
-interface ApiResponse {
-  // Success:
-  page?: number;
-  results?: (TvShow | Movie | Person)[];
-  total_pages?: number;
-  total_results?: number;
-
-  // Error:
-  status_message?: string;
-  status_code?: number;
+interface Results {
+  page: number;
+  results: (TvShow | Movie | Person)[];
+  total_pages: number;
+  total_results: number;
 }
+
+type SearchResults = Results | ApiError;
 
 const fetchSearchResults = async (title: string | undefined) => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
 
+  if (!title) return { status_code: 0, status_message: 'No title found' };
+
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${
-      title || ''
-    }&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
   );
-  const data = (await response.json()) as ApiResponse;
+  const data = (await response.json()) as SearchResults;
 
   return data;
 };
