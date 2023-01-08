0b5f076086885864cc42c8ee33d8a7e0f9f06113
Riley Loudon <rileyloudon@icloud.com>
2023-01-07 22:52:24 -0600

    append watch/providers to fetch

--- src/Components/DetailedView/Movie/fetchMovieData.ts
+++ src/Components/DetailedView/Movie/fetchMovieData.ts
@@ -1,6 +1,6 @@
-import { ApiError, Movie } from '../../../interfaces';
+import { ApiError, DetailedMovie } from '../../../interfaces';
 
-type MovieData = Movie | ApiError;
+type MovieData = DetailedMovie | ApiError;
 
 const fetchMovieData = async (movieId: string | undefined) => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
@@ -8,7 +8,7 @@ const fetchMovieData = async (movieId: string | undefined) => {
   if (!movieId) return { status_code: 0, status_message: 'No movieID found' };
 
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=watch/providers`
   );
   const movieData = (await response.json()) as MovieData;
 
