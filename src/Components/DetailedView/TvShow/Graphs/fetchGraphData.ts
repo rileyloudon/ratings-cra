448ec6cc4b3e65bfd74c813633d34b2550f1d9ef
Riley Loudon <rileyloudon@icloud.com>
2023-01-10 23:45:21 -0600

    fix fetching tv shows with more than 20 seasons

--- src/Components/DetailedView/TvShow/Graphs/fetchGraphData.ts
+++ src/Components/DetailedView/TvShow/Graphs/fetchGraphData.ts
@@ -1,20 +1,6 @@
-import { ApiError } from '../../../../interfaces';
+import { Season, ApiError } from '../../../../interfaces';
 
-interface SeasonData {
-  [seasonNumber: string]: {
-    name: string;
-    season_number: number;
-    episodes: {
-      episode_number: number;
-      name: string;
-      overview: string;
-      vote_average: number;
-      vote_count: number;
-    }[];
-  };
-}
-
-type Response = SeasonData | ApiError;
+type SeasonData = Season | ApiError;
 
 const fetchGraphData = async (tvId: number, seasons: number) => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
@@ -22,22 +8,38 @@ const fetchGraphData = async (tvId: number, seasons: number) => {
   if (!tvId || !seasons)
     return { status_code: 0, status_message: 'Unable to fetch data' };
 
+  // append_to_response has a limit of 20 sub requests
+  // need to split fetch into groups of 20 or less.
   let appendString = '';
+  const appendStringArray = [];
   for (let i = 1; i <= seasons; i += 1) {
     appendString += `season/${i},`;
+    if (i % 20 === 0 || i === seasons) {
+      appendStringArray.push(appendString);
+      appendString = '';
+    }
   }
 
-  const response = await fetch(
-    `https://api.themoviedb.org/3/tv/${tvId}?api_key=${API_KEY}&append_to_response=${appendString}`
-  );
-  const seasonData = (await response.json()) as Response;
-  if ('status_message' in seasonData) return seasonData;
-
-  const allSeasons = [];
-  for (let i = 1; i <= seasons; i += 1) {
-    const concatSeason = 'season/'.concat(`${i}`);
-    allSeasons.push(seasonData[`${concatSeason}`]);
+  const promises = [];
+  for (let i = 0; i < appendStringArray.length; i += 1) {
+    promises.push(
+      fetch(
+        `https://api.themoviedb.org/3/tv/${tvId}?api_key=${API_KEY}&append_to_response=${appendStringArray[i]}`
+      )
+    );
   }
+
+  const res = await Promise.all(promises);
+  const data = (await Promise.all(res.map((r) => r.json()))) as SeasonData[];
+
+  const combinedData = Object.assign({}, ...data) as SeasonData;
+
+  if ('status_message' in combinedData) return combinedData;
+
+  const allSeasons: Season = {};
+  Object.keys(combinedData).forEach((key) => {
+    if (key.includes('season/')) allSeasons[key] = combinedData[key];
+  });
 
   return allSeasons;
 };
