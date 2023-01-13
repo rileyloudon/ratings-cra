8c16398f7679797446737e2a73fd8d8358ba7138
Riley Loudon <rileyloudon@icloud.com>
2023-01-12 22:02:36 -0600

    update to work with removal of fetchCollectionData

--- src/Components/DetailedView/Movie/Graphs/Graphs.tsx
+++ src/Components/DetailedView/Movie/Graphs/Graphs.tsx
@@ -1,17 +1,51 @@
-import { ApiError, Movie } from '../../../../interfaces';
-import CollectionGraph from './CollectionGraph/CollectionGraph';
+import { useEffect, useState } from 'react';
+import { ApiError, DetailedMovie, Movie } from '../../../../interfaces';
+import LineGraph from '../../LineGraph/LineGraph';
+import fetchGraphData from './fetchGraphData';
+import styles from './Graphs.module.css';
 
 interface GraphsProps {
-  movieData: Movie | ApiError;
+  movieData: DetailedMovie;
 }
 
-const Graphs = ({ movieData }: GraphsProps) => (
-  <>
-    {'belongs_to_collection' in movieData &&
-      movieData.belongs_to_collection !== null && (
-        <CollectionGraph CollectionId={movieData.belongs_to_collection?.id} />
-      )}
-    {/* Other Graphs */}
-  </>
-);
+interface Collection {
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  parts: Movie[];
+}
+
+type CollectionData = Collection | ApiError;
+
+const Graphs = ({ movieData }: GraphsProps) => {
+  const [collectionData, setCollectionData] = useState<CollectionData>();
+  const [error, setError] = useState<Error>();
+
+  useEffect(() => {
+    (async (): Promise<void> => {
+      if (!collectionData && movieData.belongs_to_collection !== null) {
+        const data = await fetchGraphData(movieData.belongs_to_collection.id);
+        setCollectionData(data);
+      }
+    })().catch((err: Error) => setError(err));
+  }, [movieData.belongs_to_collection, collectionData]);
+
+  if (error) return <p>{error.message}</p>;
+  if (collectionData && 'status_message' in collectionData)
+    return <p>{collectionData.status_message}</p>;
+
+  return (
+    <>
+      <div className={styles.collection}>
+        <p>{collectionData?.name}</p>
+        {collectionData && 'parts' in collectionData && (
+          <LineGraph data={collectionData.parts} xAxisLabel='title' />
+        )}
+      </div>
+      {/* Other Graphs */}
+    </>
+  );
+};
 export default Graphs;
