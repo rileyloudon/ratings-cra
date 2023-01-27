2916e81225d289e9e30bcb04283bfc17c9d083b0
Riley Loudon <rileyloudon@icloud.com>
2023-01-26 22:41:11 -0600

    use pageNumber when fetching

--- src/Components/SearchResults/fetchSearchResults.ts
+++ src/Components/SearchResults/fetchSearchResults.ts
@@ -14,13 +14,18 @@ interface Results {
 
 type SearchResults = Results | ApiError;
 
-const fetchSearchResults = async (title: string | undefined) => {
+const fetchSearchResults = async (
+  title: string | undefined,
+  pageNumber: string | undefined
+) => {
   const API_KEY: string = process.env.REACT_APP_API_KEY!;
 
   if (!title) return { status_code: 0, status_message: 'No title found' };
+  if (!pageNumber)
+    return { status_code: 0, status_message: 'No page number found' };
 
   const response = await fetch(
-    `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${title}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${title}&page=${pageNumber}`
   );
   const data = (await response.json()) as SearchResults;
 
