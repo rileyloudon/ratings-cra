23f2b40ce830d29a6b9ee61b16201244d1fb5a59
Riley Loudon <rileyloudon@icloud.com>
2023-01-31 22:04:53 -0600

    update to work with trending data

--- src/Components/Home/Popular/Popular.tsx
+++ src/Components/Home/Popular/Popular.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Movie, TvShow } from '../../../interfaces';
+import {
+  SearchResultMovie,
+  SearchResultPerson,
+  SearchResultTv,
+} from '../../../interfaces';
 import NoPoster from '../../NoPoster/NoPoster';
 import fetchPopular from './fetchPopular';
 import styles from './Popular.module.css';
 
-type CurrentPopular = (TvShow | Movie)[] | string | undefined;
+type CurrentPopular =
+  | (SearchResultTv | SearchResultMovie | SearchResultPerson)[]
+  | string;
 
 const Popular = () => {
   const [currentPopular, setCurrentPopular] = useState<CurrentPopular>();
@@ -19,23 +25,27 @@ const Popular = () => {
 
     return (
       <div className={styles.posters}>
+        {/* use media_type to set variables */}
         {currentPopular?.map((item) => {
-          const p =
-            'name' in item
-              ? { link: 'tvshow', name: item.name }
-              : { link: 'movie', name: item.title };
+          const imgPath =
+            'profile_path' in item ? item.profile_path : item.poster_path;
+          const name = 'name' in item ? item.name : item.title;
+
+          let link = 'movie';
+          if (item.media_type === 'tv') link = 'tvshow';
+          else if (item.media_type === 'person') link = 'actor';
 
           return (
             <Link
-              to={`/${p.link}/${item.id}`}
+              to={`/${link}/${item.id}`}
               key={item.id}
               state={item}
               className={styles.item}
             >
-              {item.poster_path !== null ? (
+              {imgPath !== null ? (
                 <img
-                  src={`https://image.tmdb.org/t/p/w200/${item.poster_path}`}
-                  alt={`${p.name} Poster`}
+                  src={`https://image.tmdb.org/t/p/w300/${imgPath}`}
+                  alt={`${name} Poster`}
                 />
               ) : (
                 <NoPoster />
@@ -56,7 +66,7 @@ const Popular = () => {
 
   return currentPopular || error ? (
     <div className={styles.popular}>
-      <p>Popular Now</p>
+      <p>Popular This Week</p>
       {renderPopular()}
     </div>
   ) : null;
