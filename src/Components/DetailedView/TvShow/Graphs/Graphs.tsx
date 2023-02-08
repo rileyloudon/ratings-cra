385092f0e7e574aca2d21455f31ce7384a466fea
Riley Loudon <rileyloudon@icloud.com>
2023-02-07 23:00:49 -0600

    add previous/next buttons for shows with lots of episodes

--- src/Components/DetailedView/TvShow/Graphs/Graphs.tsx
+++ src/Components/DetailedView/TvShow/Graphs/Graphs.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ApiError, DetailedTv, Season } from '../../../../interfaces';
+import { ApiError, DetailedTv, Episodes, Season } from '../../../../interfaces';
 import LineGraph from '../../LineGraph/LineGraph';
 import fetchGraphData from './fetchGraphData';
 import styles from './Graphs.module.css';
@@ -11,13 +11,12 @@ interface GraphsProps {
 type SeasonData = Season | ApiError;
 
 const Graphs = ({ tvData }: GraphsProps) => {
+  const episodesToDisplay = 10;
+
   const [seasonData, setSeasonData] = useState<SeasonData>();
   const [seasonSelector, setSeasonSelector] = useState<string>('season/1');
-
+  const [displayedData, setDisplayedData] = useState<Episodes[]>();
   const [error, setError] = useState<Error>();
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
-    setSeasonSelector(e.target.value);
 
   useEffect(() => {
     (async (): Promise<void> => {
@@ -27,6 +26,58 @@ const Graphs = ({ tvData }: GraphsProps) => {
       }
     })().catch((err: Error) => setError(err));
   }, [tvData]);
+
+  useEffect(() => {
+    if (
+      seasonData &&
+      !('status_message' in seasonData) &&
+      seasonData[seasonSelector].episodes.length
+    )
+      setDisplayedData(
+        seasonData[seasonSelector].episodes.slice(0, episodesToDisplay)
+      );
+  }, [seasonData, seasonSelector]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
+    setSeasonSelector(e.target.value);
+
+  const handlePrevClick = () => {
+    if (seasonData && !('status_message' in seasonData) && displayedData) {
+      const { episodes } = seasonData[seasonSelector];
+
+      const endingValue = episodes.findIndex(
+        (obj) => obj.id === displayedData[0].id
+      );
+
+      setDisplayedData(
+        episodes.slice(
+          endingValue - episodesToDisplay >= 0
+            ? endingValue - episodesToDisplay
+            : 0,
+          endingValue
+        )
+      );
+    }
+  };
+
+  const handleNextClick = () => {
+    if (seasonData && !('status_message' in seasonData) && displayedData) {
+      const { episodes } = seasonData[seasonSelector];
+
+      const startingValue = episodes.findIndex(
+        (obj) => obj.id === displayedData[displayedData.length - 1].id
+      );
+
+      setDisplayedData(
+        episodes.slice(
+          startingValue + 1,
+          startingValue + episodesToDisplay + 1 <= episodes.length
+            ? startingValue + episodesToDisplay + 1
+            : episodes.length
+        )
+      );
+    }
+  };
 
   if (error) return <p>{error.message}</p>;
   if (seasonData && 'status_message' in seasonData)
@@ -39,19 +90,47 @@ const Graphs = ({ tvData }: GraphsProps) => {
 
   return (
     <div className={styles.episodes}>
-      <div className={styles.dropdown}>
-        <select name='season' id='season' onChange={handleChange}>
-          {seasonOptions.map((seasonNumber, i) => (
-            <option key={seasonNumber} value={`season/${i + 1}`}>
-              {seasonNumber}
-            </option>
-          ))}
-        </select>
-      </div>
-      {seasonData && seasonData[seasonSelector].episodes.length && (
+      <select name='season' id='season' onChange={handleChange}>
+        {seasonOptions.map((seasonNumber, i) => (
+          <option key={seasonNumber} value={`season/${i + 1}`}>
+            {seasonNumber}
+          </option>
+        ))}
+      </select>
+      {seasonData &&
+        seasonData[seasonSelector].episodes.length > episodesToDisplay && (
+          <div className={styles.nav}>
+            <button
+              disabled={
+                displayedData &&
+                displayedData[0].id ===
+                  seasonData[seasonSelector].episodes[0].id
+              }
+              type='button'
+              onClick={handlePrevClick}
+            >
+              Previous {episodesToDisplay}
+            </button>
+            <button
+              disabled={
+                displayedData &&
+                displayedData[displayedData.length - 1].id ===
+                  seasonData[seasonSelector].episodes[
+                    seasonData[seasonSelector].episodes.length - 1
+                  ].id
+              }
+              type='button'
+              onClick={handleNextClick}
+            >
+              Next {episodesToDisplay}
+            </button>
+          </div>
+        )}
+      {displayedData && (
         <LineGraph
-          data={seasonData[seasonSelector].episodes}
-          xAxisLabel='name'
+          data={displayedData}
+          xAxisDataKey='name'
+          xAxisLabel='Episode Name'
         />
       )}
     </div>
