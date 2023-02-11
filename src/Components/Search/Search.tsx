a9d8637ce205e2aab476a8170aa28f0cfdf2191b
Riley Loudon <rileyloudon@icloud.com>
2023-02-10 22:58:54 -0600

    replace non-alphanumeric characters with spaces

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -17,27 +17,30 @@ const Search = ({ currentSearch = '', updateCurrentSearch }: SearchProps) => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    if (searchString.length >= 1) {
-      updateCurrentSearch(searchString);
-      navigate(`/search=${searchString}/1`);
+    if (searchString.trim().length >= 1) {
+      // turns any non-alphanumeric characters into spaces
+      const filteredSearchString = searchString.replace(/[\W_]+/g, ' ').trim();
+
+      updateCurrentSearch(filteredSearchString);
+      navigate(`/search=${filteredSearchString}/1`);
+      if (filteredSearchString !== searchString)
+        setSearchString(filteredSearchString);
     }
   };
 
   return (
-    <div>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <input
-          type='search'
-          placeholder='Search'
-          value={searchString}
-          onChange={(e) => setSearchString(e.target.value)}
-          onFocus={handleFocus}
-          // fixes safari handleFocus not selecting
-          onMouseUp={(e) => e.preventDefault()}
-        />
-        <button type='submit'>Search</button>
-      </form>
-    </div>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        type='search'
+        placeholder='Search'
+        value={searchString}
+        onChange={(e) => setSearchString(e.target.value)}
+        onFocus={handleFocus}
+        // fixes safari handleFocus not selecting
+        onMouseUp={(e) => e.preventDefault()}
+      />
+      <button type='submit'>Search</button>
+    </form>
   );
 };
 
