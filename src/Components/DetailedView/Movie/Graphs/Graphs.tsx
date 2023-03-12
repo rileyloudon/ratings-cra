f70c482dde05e0198158ffda36f3e79d60ae84c6
Riley Loudon <rileyloudon@icloud.com>
2023-03-11 22:54:24 -0600

    display 5 movies at a time on small screens

--- src/Components/DetailedView/Movie/Graphs/Graphs.tsx
+++ src/Components/DetailedView/Movie/Graphs/Graphs.tsx
@@ -20,7 +20,10 @@ interface Collection {
 type CollectionData = Collection | ApiError;
 
 const Graphs = ({ movieData }: GraphsProps) => {
+  const moviesToDisplay = window.innerWidth > 400 ? 10 : 5;
+
   const [collectionData, setCollectionData] = useState<CollectionData>();
+  const [displayedData, setDisplayedData] = useState<Movie[]>();
   const [fetchComplete, setFetchComplete] = useState<boolean>(false);
   const [error, setError] = useState<Error | false>(false);
 
@@ -42,6 +45,53 @@ const Graphs = ({ movieData }: GraphsProps) => {
     return () => abortController.abort();
   }, [movieData.belongs_to_collection, collectionData]);
 
+  useEffect(() => {
+    if (collectionData && !('status_message' in collectionData))
+      setDisplayedData(collectionData.parts.slice(0, moviesToDisplay));
+  }, [collectionData, moviesToDisplay]);
+
+  const handlePrevClick = () => {
+    if (
+      collectionData &&
+      !('status_message' in collectionData) &&
+      displayedData
+    ) {
+      const endingValue = collectionData.parts.findIndex(
+        (obj) => obj.id === displayedData[0].id
+      );
+
+      setDisplayedData(
+        collectionData.parts.slice(
+          endingValue - moviesToDisplay >= 0
+            ? endingValue - moviesToDisplay
+            : 0,
+          endingValue
+        )
+      );
+    }
+  };
+
+  const handleNextClick = () => {
+    if (
+      collectionData &&
+      !('status_message' in collectionData) &&
+      displayedData
+    ) {
+      const startingValue = collectionData.parts.findIndex(
+        (obj) => obj.id === displayedData[displayedData.length - 1].id
+      );
+
+      setDisplayedData(
+        collectionData.parts.slice(
+          startingValue + 1,
+          startingValue + moviesToDisplay + 1 <= collectionData.parts.length
+            ? startingValue + moviesToDisplay + 1
+            : collectionData.parts.length
+        )
+      );
+    }
+  };
+
   if (error) return <p className={styles.error}>{error.message}</p>;
   if (collectionData && 'status_message' in collectionData)
     return <p className={styles.error}>{collectionData.status_message}</p>;
@@ -49,8 +99,33 @@ const Graphs = ({ movieData }: GraphsProps) => {
   return fetchComplete ? (
     <div className={styles.collection}>
       <p>{collectionData?.name || 'Standalone Movie'}</p>
+      {collectionData && collectionData.parts.length > moviesToDisplay && (
+        <div className={styles.nav}>
+          <button
+            disabled={
+              displayedData &&
+              displayedData[0].id === collectionData.parts[0].id
+            }
+            type='button'
+            onClick={handlePrevClick}
+          >
+            Previous {moviesToDisplay}
+          </button>
+          <button
+            disabled={
+              displayedData &&
+              displayedData[displayedData.length - 1].id ===
+                collectionData.parts[collectionData.parts.length - 1].id
+            }
+            type='button'
+            onClick={handleNextClick}
+          >
+            Next {moviesToDisplay}
+          </button>
+        </div>
+      )}
       <LineGraph
-        data={collectionData?.parts || [movieData]}
+        data={displayedData || [movieData]}
         xAxisDataKey='title'
         xAxisLabel='Movie Title'
         highlightDot={movieData}
