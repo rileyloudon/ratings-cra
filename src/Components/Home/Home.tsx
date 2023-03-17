f9906d3cf8de5e8c87df8f12aa30620e0e513dfb
Riley Loudon <rileyloudon@icloud.com>
2023-03-16 21:33:06 -0500

    reset search when home renders

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Search from '../Search/Search';
 import Popular from './Popular/Popular';
 import styles from './Home.module.css';
@@ -6,12 +7,18 @@ interface HomeProps {
   updateCurrentSearch(value: string): void;
 }
 
-const Home = ({ updateCurrentSearch }: HomeProps) => (
-  <div className={styles.container}>
-    <p className={styles.instructions}>Search for Movies or TV Shows</p>
-    <Search updateCurrentSearch={updateCurrentSearch} />
-    <Popular />
-  </div>
-);
+const Home = ({ updateCurrentSearch }: HomeProps) => {
+  useEffect(() => {
+    updateCurrentSearch('');
+  }, [updateCurrentSearch]);
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.instructions}>Search for Movies or TV Shows</p>
+      <Search updateCurrentSearch={updateCurrentSearch} />
+      <Popular />
+    </div>
+  );
+};
 
 export default Home;
