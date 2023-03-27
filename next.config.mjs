3729c5713837087699c40512e4e5a7eea4041a97
Riley Loudon <rileyloudon@icloud.com>
2023-03-26 23:42:47 -0500

    add next.config

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+export default nextConfig;
